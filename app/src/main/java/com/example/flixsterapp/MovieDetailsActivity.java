6b04d1d21cdbc0c527b7c30b93a744f99463000f
Fianko Buckle <kingfi@fb.com>
2020-06-25 12:46:03 -0700

    Deserialized parcel in MovieDetailsActivity

--- app/src/main/java/com/example/flixsterapp/MovieDetailsActivity.java
+++ app/src/main/java/com/example/flixsterapp/MovieDetailsActivity.java
@@ -3,12 +3,23 @@ package com.example.flixsterapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.flixsterapp.models.Movie;
+
+import org.parceler.Parcels;
 
 public class MovieDetailsActivity extends AppCompatActivity {
+
+    Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_details);
+
+        // unwrap the movie passed in via intent, using its simple name as a key
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
     }
 }
